eb186a75c2ae15f6bc59190c1033d80d831b1ab1
Adam Urban <adam.karol.urban@gmail.com>
2017-01-27 01:12:13 +0100

    Added missing semicolons

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const PublicApi = require('./publicApi')()
+const PublicApi = require('./publicApi')();
 
 const BitBayApi = () => {
   const getTrades = (currency1, currency2) => {
-    return PublicApi.makeRequest('trades', currency1, currency2)
-  }
+    return PublicApi.makeRequest('trades', currency1, currency2);
+  };
 
   const getOrderbook = (currency1, currency2) => {
-    return PublicApi.makeRequest('orderbook', currency1, currency2)
-  }
+    return PublicApi.makeRequest('orderbook', currency1, currency2);
+  };
 
   const getMarket = (currency1, currency2) => {
-    return PublicApi.makeRequest('market', currency1, currency2)
-  }
+    return PublicApi.makeRequest('market', currency1, currency2);
+  };
 
   const getTicker = (currency1, currency2) => {
-    return PublicApi.makeRequest('ticker', currency1, currency2)
-  }
+    return PublicApi.makeRequest('ticker', currency1, currency2);
+  };
 
   const getAll = (currency1, currency2) => {
-    return PublicApi.makeRequest('all', currency1, currency2)
-  }
+    return PublicApi.makeRequest('all', currency1, currency2);
+  };
 
   return {
     getTrades,
@@ -27,7 +27,7 @@ const BitBayApi = () => {
     getMarket,
     getTicker,
     getAll,
-  }
-}
+  };
+};
 
-module.exports = BitBayApi
+module.exports = BitBayApi;
