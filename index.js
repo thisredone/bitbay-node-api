40b1d60fa635dfb6b453dd8cd21fef5239e482d0
Adam Urban <adam.karol.urban@gmail.com>
2017-01-27 01:06:49 +0100

    Added basic api files

---
+++ index.js
@@ -0,0 +1,33 @@
+const PublicApi = require('./publicApi')()
+
+const BitBayApi = () => {
+  const getTrades = (currency1, currency2) => {
+    return PublicApi.makeRequest('trades', currency1, currency2)
+  }
+
+  const getOrderbook = (currency1, currency2) => {
+    return PublicApi.makeRequest('orderbook', currency1, currency2)
+  }
+
+  const getMarket = (currency1, currency2) => {
+    return PublicApi.makeRequest('market', currency1, currency2)
+  }
+
+  const getTicker = (currency1, currency2) => {
+    return PublicApi.makeRequest('ticker', currency1, currency2)
+  }
+
+  const getAll = (currency1, currency2) => {
+    return PublicApi.makeRequest('all', currency1, currency2)
+  }
+
+  return {
+    getTrades,
+    getOrderbook,
+    getMarket,
+    getTicker,
+    getAll,
+  }
+}
+
+module.exports = BitBayApi
---
+++ publicApi.js
@@ -0,0 +1,62 @@
+const request = require('request-promise')
+
+const PublicApi = (currencies) => {
+  const apiAddress = 'https://bitbay.net/API/Public'
+
+  const categories = [
+    'trades',
+    'orderbook',
+    'market',
+    'ticker',
+    'all',
+  ]
+
+  const buildAddress = (category, currency1, currency2) => {
+    return `${apiAddress}/${currency1}${currency2}/${category}.json`
+  }
+
+  const validateCategory = (category) => {
+    if (categories.indexOf(category) === -1) {
+      throw new Error(`Category ${category} is invalid`)
+    }
+  }
+
+  const validateRequest = (category, currency1, currency2) => {
+    if (category == null) {
+      throw new Error('Category is required')
+    }
+
+    if (currency1 == null) {
+      throw new Error('First currency is required')
+    }
+
+    validateCategory(category)
+
+    if (currency1 === currency2) {
+      throw new Error('First currency must be different than second currency')
+    }
+  }
+
+  const makeRequest = (category, currency1, currency2 = 'USD') => {
+    try {
+      validateRequest(category, currency1, currency2);
+    } catch (err) {
+      return Promise.reject(err);
+    }
+
+    const address = buildAddress(category, currency1, currency2)
+
+    return request.get(address)
+  }
+
+  const getCategories = () => {
+    return Object.assign({ }, categories)
+  }
+
+  return {
+    makeRequest,
+    getCategories,
+  }
+}
+
+module.exports = PublicApi;
